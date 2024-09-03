332e1c3ffb919f17df0fefefc16a53d9457b5862
BISD4 <uct-pages@uct.edu.pe>
2024-09-03 12:58:36 -0500

    bbb

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const sombreros = document.querySelectorAll('.sombrero');
+
+sombreros.forEach(sombrero => {
+    // Asigna una posición inicial aleatoria
+    sombrero.style.left = Math.random() * window.innerWidth - sombrero.offsetWidth + 'px';
+    sombrero.style.top = Math.random() * window.innerHeight - sombrero.offsetHeight + 'px';
+
+    // Personaliza la animación de cada sombrero
+    sombrero.style.animationDelay = Math.random() * 2 + 's';
+});
+
+/*
 function chooseOption(option) {
     document.getElementById('home').classList.add('hidden');
     document.getElementById('animation').classList.remove('hidden');
@@ -19,3 +31,5 @@ function scrollToNext(pageId) {
         behavior: 'smooth'
     });
 }
+
+*/
